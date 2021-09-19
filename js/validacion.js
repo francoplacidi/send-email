485575d1a1baff97510a1184ca5a4c91d1502bcd
Franco Nicolás Placidi <79880040+francoplacidi@users.noreply.github.com>
2021-09-18 22:21:36 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,38 @@ function validarCorreo(correo){
         alert("El correo NO es válido!");
     }
 }
+
+function habilitar(){
+    nombre = document.getElementById("nombre").value;
+    correo = document.getElementById("correo").value;
+    asunto = document.getElementById("asunto").value;
+    msg = document.getElementById("msg").value;
+    val = 0;
+
+    if(nombre == ""){
+        val++;
+    }
+    if(correo == ""){
+        val++;
+    }
+    if(asunto == ""){
+        val++;
+    }
+    if(msg == ""){
+        val++;
+    }
+    if(val == 0){
+        document.getElementById("btn").disabled = false;
+    }
+    else{
+        document.getElementById("btn").disabled = true;
+    }
+}
+
+document.getElementById("nombre").addEventListener("keyup", habilitar);
+document.getElementById("correo").addEventListener("keyup", habilitar);
+document.getElementById("asunto").addEventListener("keyup", habilitar);
+document.getElementById("msg").addEventListener("keyup", habilitar)
+document.getElementById("btn").addEventListener("click", () => {
+    console.log("Se ha completado y enviado el formulario !!");
+});
